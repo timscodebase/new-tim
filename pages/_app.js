c5039d5fb50497a1367ca9dadd62b03dcc762b71
Tim Smith <tim.smith.hdg@gmail.com>
2020-12-22 18:01:30 -0800

    web vitals

--- pages/_app.js
+++ pages/_app.js
@@ -16,3 +16,26 @@ export default function MyApp({ Component, pageProps }) {
     </>
   )
 }
+
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case 'FCP':
+      break
+    case 'LCP':
+      break
+    case 'CLS':
+      break
+    case 'FID':
+      break
+    case 'TTFB':
+      break
+    case 'Next.js-hydration':
+      break
+    case 'Next.js-route-change-to-render':
+      break
+    case 'Next.js-render':
+      break
+    default:
+      break
+  }
+}
