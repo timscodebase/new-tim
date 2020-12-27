ded85102cab63cc5832c73eed2fa106f250aee2c
Tim Smith <tim.smith.hdg@gmail.com>
2020-12-26 19:18:40 -0800

    instagram

--- components/Photography/index.js
+++ components/Photography/index.js
@@ -1,4 +1,4 @@
-import Instagram from '../Instagram'
+import Feed from 'react-instagram-authless-feed'
 import styles from './Photography.module.css'
 
 export default function Photography() {
@@ -7,7 +7,12 @@ export default function Photography() {
       <h1>Photography</h1>
       <h2 className={styles.h2}>Hand Picked</h2>
       <h2 className={styles.h2}>Instagram Snaps</h2>
-      <Instagram />
+      <Feed
+        userName="tithos"
+        className={styles.gramz}
+        classNameLoading="Loading"
+        limit="6"
+      />
     </div>
   )
 }
